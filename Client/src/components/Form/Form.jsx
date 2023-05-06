1ca5e75f241e4e50ddf9a46dffb6d81e043abe63
resekk <sebaresekk@gmail.com>
2023-05-06 00:37:59 -0300

    seba resek

--- Client/src/components/Form/Form.jsx
+++ Client/src/components/Form/Form.jsx
@@ -1,47 +1,83 @@
 import { useState } from "react";
 import validation from "./Validation";
 import style from "./Form.module.css";
-// import fondo from '../assets/img/fondo.png'
+import axios from "axios";
 
 const Form = ({ login }) => {
-    const [errors, setErrors] = useState({});
-    const [userData, setUserData] = useState({
-        email: '',
-        password: ''
+  const [errors, setErrors] = useState({});
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+  const [isRegistering, setIsRegistering] = useState(false); // Nueva variable de estado para saber si se está registrando
+
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
     });
 
-    const handleChange = (event) => {
-        setUserData({
-            ...userData,
-            [event.target.name]: event.target.value
+    setErrors(
+      validation({
+        ...userData,
+        [event.target.name]: event.target.value,
+      })
+    );
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isRegistering) { // Si se está registrando, usa axios.post
+      axios
+        .post("http://localhost:3001/rickandmorty/login/register", {
+          password: userData.password,
+          email: userData.email,
+          id: 1,
         })
-
-        setErrors(validation({
-            ...userData,
-            [event.target.name]: event.target.value
-        }))
+        .then(({ data }) => {
+          console.log(data);
+          // Podrías redirigir al usuario a la página de inicio de sesión aquí
+        });
+    } else { // Si no, usa axios.get
+      login(userData);
     }
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        login(userData);
-    }
+  const handleRegisterClick = () => {
+    setIsRegistering(true);
+  };
 
-    return(
-        <div className={style.formContainer} >
-            <form className={style.form} onSubmit={handleSubmit}>
-                <label htmlFor="email">Email: </label>
-                <input type="email" name='email' value={userData.email} onChange={handleChange}/>
-            {errors.email && <p style={{ color: "red"}}>{errors.email}</p>}
-                <br/>
-                <label htmlFor="password">Password: </label>
-                <input type="password" name="password" value={userData.password} onChange={handleChange}/>
-            {errors.password && <p style={{ color: "red"}}>{errors.password}</p>}
+  return (
+    <div className={style.formContainer}>
+      <form className={style.form} onSubmit={handleSubmit}>
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          name="email"
+          value={userData.email}
+          onChange={handleChange}
+        />
+        {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          name="password"
+          value={userData.password}
+          onChange={handleChange}
+        />
+        {errors.password && (
+          <p style={{ color: "red" }}>{errors.password}</p>
+        )}
 
-                <button>LOGIN</button>
-            </form>
-        </div>
-    )
-}
+        <button>{isRegistering ? "REGISTER" : "LOGIN"}</button>
+        {!isRegistering && (
+        <button onClick={handleRegisterClick}>REGISTER</button>
+      )}
+      </form>
+      
+    </div>
+  );
+};
 
 export default Form;
--- Server/src/controllers/postUser.js
+++ Server/src/controllers/postUser.js
@@ -1,7 +1,21 @@
 const { User } = require("../DB_connection");
 
 const postUser = async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password, id} = req.body;
+    console.log(":::::::::::::::::", id, email, password);
+
+  //   try {
+  //     if (!password || !email) {
+  //       res.status(400).send('Faltan datos');
+  //     }
+  //     const user = await User.create({ id, email, password });
+  //     res.status(200).json(user);
+  //   } catch (error) {
+  //       res.status(500).send(error.message);
+  //   }
+  // }
+
+
     if (!email || !password) {
         return res.status(400).send('Faltan datos')
     }
